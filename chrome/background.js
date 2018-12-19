89444ca58d68bb3291f4516b1739ad6d1c1314aa
mitchmoser <moser.mitch@yahoo.com>
2018-12-19 00:17:07 -0800

    removed a test function from chrome extension

--- chrome/background.js
+++ chrome/background.js
@@ -241,38 +241,11 @@ chrome.contextMenus.create({
 // create empty url variable
 var url = ""
 
-// fuction to copy selected text to clipboard
-function copyTextToClipboard(text) {
-    //Create a textbox field where we can insert text to.
-    //var copyFrom = document.createElement("textarea");
-
-    //Set the text content to be the text you wished to copy.
-    copyFrom.textContent = text;
-
-    //Append the textbox field into the body as a child.
-    //"execCommand()" only works when there exists selected text, and the text is inside
-    //document.body (meaning the text is part of a valid rendered HTML element).
-    document.body.appendChild(copyFrom);
-
-    //Select all the text!
-    copyFrom.select();
-
-    //Execute command
-    document.execCommand('copy');
-
-    //(Optional) De-select the text using blur().
-    copyFrom.blur();
-
-    //Remove the textbox field from the document.body, so no other JavaScript nor
-    //other elements can get access to this.
-    document.body.removeChild(copyFrom);
-}
-
 /*
  * Source:
  * https://stackoverflow.com/questions/13899299/write-text-to-clipboard#18258178
  */
-function copyStringToClipboard (str) {
+function copyStringToClipboard(str) {
     // Create new element
     var el = document.createElement('textarea');
     // Set value (string to be copied)
